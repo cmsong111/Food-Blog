a13ea13b607c7af0317d4cedaea8e8831d8aa1a9
Namju Kim <cmsong111@naver.com>
2023-05-03 23:20:04 +0900

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into Users(email, account_non_expired, account_non_locked, credentials_non_expired, enabled, nickname, password, username)
+insert into users(email, account_non_expired, account_non_locked, credentials_non_expired, enabled, nickname, password, username)
 values ('test1@test.com', 1, 1, 1, 1, 'nickname1', 'test1', 'username1'),
        ('test2@test.com', 1, 1, 1, 1, 'nickname2', 'test2', 'username2'),
        ('test3@test.com', 1, 1, 1, 1, 'nickname3', 'test3', 'username3'),
